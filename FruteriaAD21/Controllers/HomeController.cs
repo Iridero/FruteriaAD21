19c15b70cf44990bedbca700473201f6ac23e7e3
Iridero <45569252+Iridero@users.noreply.github.com>
2021-11-24 18:52:25 -0600

    Categoria

--- FruteriaAD21/Controllers/HomeController.cs
+++ FruteriaAD21/Controllers/HomeController.cs
@@ -3,7 +3,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using FruteriaAD21.Repositories;
+using FruteriaAD21.Models;
 namespace FruteriaAD21.Controllers
 {
     public class HomeController : Controller
@@ -11,6 +12,14 @@ namespace FruteriaAD21.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        [Route("{id}")]
+        public IActionResult Categoria(string id)
+        {
+            ViewBag.Categoria = id;
+            var pr = new ProductosRepository();
+            return View(pr.GetAll());
         }
     }
 }
---
+++ FruteriaAD21/Repositories/ProductosRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FruteriaAD21.Models;
+
+namespace FruteriaAD21.Repositories
+{
+    public class ProductosRepository:Repository<Producto>
+    {
+    }
+}
